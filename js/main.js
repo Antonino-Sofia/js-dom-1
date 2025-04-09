4b3af4cc9a90d5239d5db4bf34f8ac3d869c0149
Antonino-Sofia <antoninosofia420@gmail.com>
2025-04-09 16:29:24 +0200

    js-dom-1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 console.log("Antonino");
-//Consegna
 //Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
 //Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
 //BONUS
@@ -15,14 +14,31 @@ console.log(bulbElement);
 buttonElement = document.getElementById("switch");
 console.log(buttonElement);
 //esercizio di default
-buttonElement.addEventListener('click', function () {
-console.log("click bottone");
+buttonElement.addEventListener("click", function () {
+  console.log("click bottone");
 
-console.log(bulbElement.src);
-console.log(bulbElement.id);
-console.log(bulbElement.alt);
+  console.log(bulbElement.src);
+  console.log(bulbElement.id);
+  console.log(bulbElement.alt);
 
-bulbElement.src = "img/yellow_lamp.png";
-bulbElement.alt = "Lamapadina accesa";
+  bulbElement.src = "img/yellow_lamp.png";
+  bulbElement.alt = "Lamapadina accesa";
+});
 //BONUS
+document.getElementById("switch").addEventListener("click", accendiLampadina);
 
+function accendiLampadina() {
+  console.log("accendi lampadina");
+  const bulbElement = document.getElementById("bulb");
+  console.log(bulbElement);
+  //BOTTONE
+  const buttonElement = document.getElementById("switch");
+  console.log(buttonElement);
+
+  bulbElement.src = "img/yellow_lamp.png";
+  bulbElement.alt = "Lampadina accesa";
+  buttonElement.innerText = "Spegni";
+
+  buttonElement.removeEventListener("click", accendiLampadina);
+  buttonElement.addEventListener("click", spegniLampadina);
+}
